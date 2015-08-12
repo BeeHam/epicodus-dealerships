a477bbacac01c7fe0ee51d639ad7aceb6111e6ec
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-08-12 11:12:16 -0700

    Add age spec and method for vehicle.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -26,4 +26,13 @@ class Vehicle
   define_singleton_method(:all) do
     @@vehicles
   end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:age) do
+    current_year = Time.new().year()
+    age = current_year.-(@year)
+  end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,9 +2,9 @@ require('rspec')
 require('vehicle')
 
 describe('Vehicle') do
-  # before() do
-  #   Vehicle.clear()
-  # end
+  before() do
+    Vehicle.clear()
+  end
 
   describe('#make') do
     it("returns the make of the vehicle") do
@@ -35,5 +35,20 @@ describe('Vehicle') do
       expect(Vehicle.all()).to(eq([]))
     end
   end
-  
+
+  describe('.clear') do
+    it("empties out all of the saved vehicles") do
+        Vehicle.new("Toyota", "Prius", 2000).save()
+        Vehicle.clear()
+        expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#age') do
+    it("returns the vehicles age") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.age()).to(eq(15))
+    end
+  end
+
 end
