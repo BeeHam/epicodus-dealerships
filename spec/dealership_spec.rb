044b1e2a4eb15ebee2c3bad88cafa3a1e51baf3a
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-08-12 09:43:24 -0700

    adds README, spec for name and id methods

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,22 @@
+require('rspec')
+require('dealership')
+
+describe(Dealership) do
+  # before() do
+  #   Dealership.clear()
+  # end
+
+  describe('#name') do
+    it("returns the name of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+    end
+  end
+
+  describe('#id') do
+    it("returns the id of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+end
