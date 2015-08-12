9ab7ebae95cc63c3bc3ec912710ca958119cd8e9
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-08-12 10:04:26 -0700

    adds spec tests: save & all. adds methods: save & all

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,6 +4,7 @@ class Dealership
   define_method(:initialize) do |name|
     @name = name
     @id = @@dealerships.length().+(1)
+    @cars = []
   end
 
   define_method(:name) do
@@ -12,6 +13,18 @@ class Dealership
 
   define_method(:id) do
     @id
+  end
+
+  define_method(:cars) do
+    @cars
+  end
+
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+
+  define_method(:save) do
+    @@dealerships.push(self)
   end
 
 end
