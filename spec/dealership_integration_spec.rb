ef6cebd8517ca30902eeb8511b978eb4180e0826
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-08-12 16:11:37 -0700

    adds specs for viewing cars in dealership

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -30,3 +30,29 @@ describe('path for dealerships form', {:type => :feature}) do
     expect(page).to have_content("Success")
   end
 end
+
+describe('path to dealership list', {:type => :feature}) do
+  it('sends user to dealership list') do
+    visit('/dealerships/new')
+    fill_in("name", :with => "Janice's")
+    click_button('Add Dealership')
+    click_link('See Dealership List')
+    expect(page).to have_content("Dealerships")
+  end
+end
+
+describe("path to a specific dealership's list of cars", {:type => :feature}) do
+  it('sends user to list of cars') do
+    visit('/dealerships')
+    click_link("Bob's Used Cars")
+    expect(page).to have_content("Here are all the cars in this dealership:")
+  end
+end
+
+describe("path to add new vehicle to a dealership", {:type => :feature}) do
+  it('processes user entry and adds new vehicle') do
+    visit('/dealerships/1')
+    click_link("Add a new vehicle")
+    expect(page).to have_content("Add a vehicle to Bob's Used Cars")
+  end
+end
